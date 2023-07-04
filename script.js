3e34e1914d06f663efbe41cc74b233be043c051d
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-04 13:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your code here
+function findMajorityElement(arr) {
+  let majorityElement;
+  let count = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (count === 0) {
+      majorityElement = arr[i];
+      count = 1;
+    } else if (majorityElement === arr[i]) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return majorityElement;
+}
